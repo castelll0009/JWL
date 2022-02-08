85ef6c7634e221282974a1d6f402db333cdaa688
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-02-08 11:56:09 -0500

    Funcionando el calculo basico

--- js/simulador-inversiones.js
+++ js/simulador-inversiones.js
@@ -3,20 +3,28 @@ let  valor_invertir = 0; //valor   invertir en dolares
 let ganancias_plazo = 0;
 let meses_inversion = 0;
 
-/*
-$(document).on('click', '.boton-delete', function() {
+$('#formulario-simulador-inversion').submit(e => {        
+  e.preventDefault();     
 
-  //antes  de  calcular  comprobar  si  los  campos estan llenos
-   
+  //antes  de  calcular  comprobar  si  los  campos estan llenos   
     valor_invertir =  $('#id-valor-inversion').val();
     meses_inversion =  $('#select-meses-inversion').val();
-    ganancias_plazo = valor_invertir * interes;
-   
-     
+    ganancias_plazo = valor_invertir * interes;       
+    $("#id-resultado-inversion").val(ganancias_plazo + " USD");
+    alert(ganancias_plazo);
 });
-*/
+
 
 $(document).on('click', '.simulador-inversiones' , function(){                
     //despliegue detalles con toggle        
     $(".div-interfaz-simulador").toggleClass("desplegar-hacia-izquierda");        
-});    
+});  
+
+//funciones para onmouseover y onmouseout
+function bigImg(x) {
+  x.style= "background-color :#00AFFF";   
+}
+
+function normalImg(x) {
+  x.style= "background-color: #d682b06e;";   
+}
